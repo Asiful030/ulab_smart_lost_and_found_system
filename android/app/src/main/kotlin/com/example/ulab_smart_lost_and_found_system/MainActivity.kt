3e37f3af131185bf48348c529f6374b7cad61775
asiful030 <asiful.islam.cse@ulab.edu.bd>
2025-08-26 22:26:05 +0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/ulab_smart_lost_and_found_system/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ulab_smart_lost_and_found_system
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:ulab_smart_lost_and_found_system/auth/login_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Ulab Smart Lost and Found System',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: LoginScreen(),
+    );
+  }
+}
