42df2aa6d94a48eb4233bd8c4b7d86a0a2222679
rafia_haque_arpita <rafia.haque.cse@ulab.edu.bd>
2025-09-07 01:34:51 +0600

    SignUp_screen created

--- lib/auth/login_screen.dart
+++ lib/auth/login_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:ulab_smart_lost_and_found_system/auth/login_screen.dart';
 
-class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
+class SignUpScreen extends StatelessWidget {
+  const SignUpScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -21,7 +22,7 @@ class LoginScreen extends StatelessWidget {
               children: [
                 SizedBox(height: 100),
                 Text(
-                  "Ulab Smart Lost and Found System \n Management System",
+                  "Ulab Smart Lost and Found \n Management System",
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     fontSize: 20,
@@ -32,7 +33,7 @@ class LoginScreen extends StatelessWidget {
                 SizedBox(height: 100),
                 TextField(
                   decoration: InputDecoration(
-                    hintText: "ULAB ID",
+                    hintText: "Name",
                     filled: true,
                     prefixIcon: Icon(Icons.person),
                     fillColor: Colors.white.withOpacity(0.5),
@@ -50,12 +51,54 @@ class LoginScreen extends StatelessWidget {
                     ),
                   ),
                 ),
-                SizedBox(height: 20),
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: "ULAB ID",
+                    filled: true,
+                    prefixIcon: Icon(Icons.perm_identity),
+                    fillColor: Colors.white.withOpacity(0.5),
+                    hintStyle: TextStyle(color: Colors.black),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                  ),
+                ),
+                // SizedBox(height: 20),
                 TextField(
                   obscureText: true,
 
                   decoration: InputDecoration(
-                    hintText: "Password",
+                    hintText: "Enter your password",
+                    filled: true,
+                    prefixIcon: Icon(Icons.password),
+                    fillColor: Colors.white.withOpacity(0.5),
+                    hintStyle: TextStyle(color: Colors.black),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                  ),
+                ),
+                TextField(
+                  obscureText: true,
+
+                  decoration: InputDecoration(
+                    hintText: "Enter Your Password Again",
                     filled: true,
                     prefixIcon: Icon(Icons.password),
                     fillColor: Colors.white.withOpacity(0.5),
@@ -78,30 +121,34 @@ class LoginScreen extends StatelessWidget {
                   width: 200,
                   child: ElevatedButton(
                     onPressed: () {},
-                    child: Text("Login", style: TextStyle(color: Colors.black)),
                     style: ElevatedButton.styleFrom(
                       backgroundColor: Colors.blue,
                     ),
+                    child: Text(
+                      "Sign Up",
+                      style: TextStyle(color: Colors.black),
+                    ),
                   ),
                 ),
-                TextButton(
-                  onPressed: () {},
-                  child: Text(
-                    'Forgot Password?',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
+
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Text(
-                      'Don\'t have an account? ',
+                      'Do you already have account? ',
                       style: TextStyle(color: Colors.white),
                     ),
                     TextButton(
-                      onPressed: () {},
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => LoginScreen(),
+                          ),
+                        );
+                      },
                       child: Text(
-                        'Sign Up',
+                        'Login',
                         style: TextStyle(
                           fontWeight: FontWeight.bold,
                           color: Colors.white,
